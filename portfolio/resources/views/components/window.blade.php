82b9737464153c14645ec888c6efaa8379e2b6db
Sebastian <sebastiangioiose@gmail.com>
2025-02-18 15:22:08 -0300

    first tower commit

---
+++ portfolio/resources/views/components/window.blade.php
@@ -0,0 +1,3 @@
+<div style="background-color: green;">
+    <img src="{{ asset('/img/tower2.png')}}" alt="window" style="height: 7.313rem; width: 15.5rem;">
+</div>
--- portfolio/resources/views/projects.blade.php
+++ portfolio/resources/views/projects.blade.php
@@ -1,6 +1,18 @@
 @include('layouts.header')
 <body style="background-image: url('{{ asset('/img/spiral.png') }}'); background-size: cover; background-position: center center;">
 
+    <a href="/select"><img src="{{ asset("img/back.png")}}" class="ml-2" alt="back"></a>
+    {{-- TO DO fix scroll issue --}}
+    <div class="flex flex-col items-center justify-end h-screen" style="align-items: center;">
+        <button><x-window></x-window></button>
+        <button><x-window></x-window></button>
+        <button><x-window></x-window></button>
+        <button><x-window></x-window></button>
+        <button><x-window></x-window></button>
+        <button><x-window></x-window></button>
+        <img src="{{ asset('img/tower1.png') }}" alt="base" style="height: 7.313rem; width: 28.938rem;">
+        {{-- TO DO fix alignment to bottom img--}}
+    </div>
 
 
 @include('layouts.footer')
