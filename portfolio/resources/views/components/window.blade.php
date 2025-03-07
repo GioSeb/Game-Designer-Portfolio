e64040f3c730b95c6366b5ae62376f844c827154
Sebastian <sebastiangioiose@gmail.com>
2025-03-07 11:49:56 -0300

    finished projects tower

--- portfolio/resources/views/components/window.blade.php
+++ portfolio/resources/views/components/window.blade.php
@@ -1,36 +1,24 @@
-{{-- TO DO fix margin between the component --}}
-<div x-data="{ isOpen: false }">
-    <!-- Button to open the modal -->
-    <button @click="isOpen = true">
-        <div style="background-color: green;"> {{-- TO DO add transparent png with project --}}
-            <img src="{{ asset('/img/tower2.png')}}" alt="window" style="height: 7.313rem; width: 15.5rem;">
-        </div>
-    </button>
-    {{-- TO DO finish modal --}}
-    <!-- Modal backdrop and content -->
-    <div
-        x-show="isOpen"
-        class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center"
-        @click.away="isOpen = false"
-    >
-        <!-- Modal content with animation -->
-        <div
-            x-show="isOpen"
-            x-transition:enter="motion-safe:ease-out duration-300"
-            x-transition:enter-start="opacity-0 scale-90"
-            x-transition:enter-end="opacity-100 scale-100"
-            class="max-w-3xl p-6 bg-white rounded shadow-lg"
-        >
-            <!-- Modal content here -->
-            <h3 class="text-lg font-medium leading-6 text-gray-900">Modal Title</h3>
-            <div class="mt-2">
-                <p class="text-sm leading-5 text-gray-500">Modal content goes here.</p>
-            </div>
-            <!-- Close button -->
-            <button @click="isOpen = false" class="inline-flex justify-center w-full px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700">Close Modal</button>
-        </div>
+{{-- TO DO fix space to CHOOSE YOUR FIGHTER --}}
+<div x-data="{ isOpen: false, towerCount: 5 }">
+    <div x-show="isOpen" class="absolute w-1/2 bg-red-500" style="top: 20rem; left: 50px; margin-left: 5rem; width: 36.25rem; height: 38rem;">
+      <img src="{{ asset('/img/text/projectTitle.png')}}" alt="" class="mx-auto mt-4">
     </div>
-</div>
+
+    <div class="relative flex flex-col items-center justify-end h-screen mx-auto my-0 p-0" style="width: 28.938rem;">
+      <div class="flex flex-col items-center justify-center">
+        <template x-for="(tower, index) in Array(towerCount).fill(0)" :key="index">
+          <button v-if="index === towerCount - 1" @click="isOpen = !isOpen">
+            <div class="mx-auto" style="background-color: green;">
+              <img src="{{ asset('/img/tower2.png')}}" alt="window" style="height: 7.313rem; width: 15.5rem;">
+            </div>
+          </button>
+          <div v-else class="mx-auto" style="background-color: green;">
+            <img src="{{ asset('/img/tower2.png')}}" alt="window" style="height: 7.313rem; width: 15.5rem;">
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
 
 {{-- <button>
     <div style="background-color: green;">
--- portfolio/resources/views/projects.blade.php
+++ portfolio/resources/views/projects.blade.php
@@ -1,18 +1,17 @@
 @include('layouts.header')
+
+{{-- TO DO fix alignment between tower2 and tower1 --}}
+
 <body style="background-image: url('{{ asset('/img/spiral.png') }}'); background-size: cover; background-position: center center;">
 
-    <a href="/select"><img src="{{ asset("img/back.png")}}" class="ml-2" alt="back"></a>
-    {{-- TO DO fix scroll issue --}}
-    <div class="flex flex-col items-center justify-end h-screen" style="align-items: center;">
-            <x-window></x-window>
-            <x-window></x-window>
-            <x-window></x-window>
-            <x-window></x-window>
-            <x-window></x-window>
-            <x-window></x-window>
-        <img src="{{ asset('img/tower1.png') }}" alt="base" style="height: 7.313rem; width: 28.938rem;">
-        {{-- TO DO fix alignment to bottom img--}}
-    </div>
+  <a href="/select"><img src="{{ asset("img/back.png")}}" class="ml-2" alt="back"></a>
+  <img src="{{ asset('/img/text/choose.png')}}" alt="https://www.textstudio.com/ Font generator" class="mx-auto">
 
+  <div class="flex flex-col items-center justify-end h-auto mx-auto">
+    <div class="flex flex-col items-center justify-center h-screen">
+      <x-window></x-window>
+      <img src="{{ asset('/img/tower1.png')}}" alt="tower1" style="height: 7.313rem; width: 28.938rem;">
+    </div>
+  </div>
 
 @include('layouts.footer')
